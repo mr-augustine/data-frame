8bd411e4ba6af9861e6920c887c9427e062fb22c
mr-augustine <pushbuttonrobotics@gmail.com>
2015-10-06 15:14:39 -0500

    Added error message support; fixed typo

---
+++ data/server/basic/models/Messages.class.php
@@ -0,0 +1,32 @@
+<?php
+class Messages {
+    public static $errors = array();
+    public static $locale = "English";
+
+    public static function setErrors($filename) {
+        $errorsArray = array();
+
+        // TODO verify that the file exists first!
+        foreach (file($filename) as $line) {
+            list($key, $value) = explode(' ', $line, 2) + array(NULL, NULL);
+
+            if ($value !== NULL)
+                $errorsArray[$key] = $value;
+        }
+
+        self::$errors = $errorsArray;
+    }
+
+    public static function getError($errorName) {
+        if (isset(self::$errors, $errorName))
+            return self::$errors[$errorName];
+        else
+            return "";
+    }
+
+    public static function reset() {
+        $pathDir = dirname(__FILE__);
+        $fileName = $pathDir . DIRECTORY_SEPARATOR . "../resources/errors_" . self::$locale . ".txt";
+    {
+}
+?>
