066d4d39d5ef80dc361557f5b2140cbf715df268
mr-augustine <pushbuttonrobotics@gmail.com>
2015-10-05 19:03:59 -0500

    Added prelim Sensor class

---
+++ data/server/basic/models/Sensor.class.php
@@ -0,0 +1,138 @@
+<?php
+class Sensor {
+    public static $SENSOR_TYPES = array("temperature");
+    public static $UNITS = array("degrees_C" => "temperature",
+                                "degrees_F" => "temperature");
+
+    private static $DESCRIP_MAX_LENGTH = 255;
+    private static $NAME_MAX_LENGTH = 255;
+
+    private $errorCount;
+    private $errors;
+    private $formInput;
+
+    private $description;
+    private $name;
+    private $type;
+
+    public function __construct($formInput = null) {
+        $this->formInput = $formInput;
+        Messages::reset();
+        $this->initialize();
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getError($errorName) {
+        if (isset($this->errors[$errorName]))
+            return $this->errors[$errorName];
+        else
+            return "";
+    }
+
+    public function setError($errorName, $errorValue) {
+        $this->errors[$errorName] = Messages::getError($errorValue);
+        $this->errorCount++;
+    }
+
+    public function getErrorCount() {
+        return $this->errorCount;
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function __toString() {
+        $str = "[" . get_class($this) . ": name=" . $this->getName() . ", type=" .
+                $this->getType() . ", description=" . $this->getDescription() .
+                "]";
+
+        return $str;
+    }
+
+    private function extractForm($valueName) {
+        $value = "";
+
+        if (isset($this->formInput[$valueName])) {
+            $value = trim($this->formInput[$valueName]);
+            $value = stripslashes($value);
+            $value = htmlspecialchars($value);
+
+            return $value;
+        }
+    }
+
+    private function initialize() {
+        $this->errorCount = 0;
+        $this->errors = array();
+
+        if (is_null($this->formInput))
+            $this->initializeEmpty();
+        else {
+            $this->validateName();
+            $this->validateType();
+            $this->validateDescription();
+        }
+    }
+
+    private function initializeEmpty() {
+        $this->errorCount = 0;
+        $this->errors = array();
+        $this->description = "";
+        $this->name = "";
+        $this->type = "";
+    }
+
+    private function validateDescription() {
+        // Description is an optional field
+        // If set, length cannot exceed SENSOR_DESCRIP_TOO_LONG
+        $this->description = $this->extractForm('description');
+
+        if (!empty($this->description)) {
+            if (count($this->description) > self::$DESCRIP_MAX_LENGTH)
+                $this->setError('description', 'SENSOR_DESCRIP_TOO_LONG');
+        }
+    }
+
+    private function validateName() {
+        // Name is a mandatory field
+        // It may only contain letters, numbers, dashes, and underscores
+        $this->name = $this->extractForm('name');
+
+        if (empty($this->name))
+            $this->setError('name', 'SENSOR_NAME_EMPTY');
+        elseif (count($this->name) > self::$NAME_MAX_LENGTH)
+            $this->setError('name', 'SENSOR_NAME_TOO_LONG');
+        elseif (!filter_var($this->name, FILTER_VALIDATE_REGEXP,
+            array("options"=>array("regexp" => "/^[a-zA-z0-9-_]+$/")) )) {
+            $this->setError('name', 'SENSOR_NAME_INVALID');
+        }
+    }
+
+    private function validateType() {
+        // Type is a mandatory field
+        // It may only be one of the predefined types
+                
+        //TODO flesh out the details
+    }
+
+    private function validateUnits() {
+        // Units is a mandatory field
+        // It/They must mach with the predefined type
+
+        //TODO flesh out the details
+    }
+}
+
+?>
